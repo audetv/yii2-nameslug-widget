9d70b562cbcccd2ce5674dc578255859d67b4634
Alexey Gusev <audetv@gmail.com>
2019-12-13 14:53:07 +0300

    changed js transliteration table

--- src/assets/js/name-slug.js
+++ src/assets/js/name-slug.js
@@ -8,9 +8,9 @@ function process(nameField, slugField){
         text = $('#'+ nameField).val().toLowerCase(),
         transliteration = {
             'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'e', 'ж': 'zh',
-            'з': 'z', 'и': 'i', 'й': 'j', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n',
-            'о': 'o', 'п': 'p', 'р': 'r','с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h',
-            'ц': 'c', 'ч': 'ch', 'ш': 'sh', 'щ': 'sh','ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya',
+            'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n',
+            'о': 'o', 'п': 'p', 'р': 'r','с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh',
+            'ts': 'c', 'ч': 'ch', 'ш': 'sh', 'щ': 'sch','ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya',
             ' ': space, '_': space, '`': space, '~': space, '!': space, '@': space,
             '#': space, '$': space, '%': space, '^': space, '&': space, '*': space,
             '(': space, ')': space,'-': space, '\=': space, '+': space, '[': space,
